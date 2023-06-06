169f4f61b86ddd565b840a3fcd6421856d5fca2a
inpsyde-deploybot <github-deploybot@inpsyde.com>
2023-06-06 11:04:44 +0000

    [SYNC] Fix preview link (#93)

--- templates/search/item-preview.php
+++ templates/search/item-preview.php
@@ -29,13 +29,6 @@ $titleId = sprintf('degree-program-title-%d', $degreeProgram->id());
 
 <li class="c-degree-program-preview">
     <div class="c-degree-program-preview__teaser-image">
-        <a
-            class="c-degree-program-preview__link"
-            href="<?= esc_url($link) ?>"
-            rel="bookmark"
-            aria-labelledby="<?= esc_attr($titleId) ?>"
-        >
-        </a>
         <?php // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped ?>
         <?= $renderer->render(
             'common/image',
@@ -53,16 +46,23 @@ $titleId = sprintf('degree-program-title-%d', $degreeProgram->id());
         <?php // phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped ?>
     </div>
 
-    <div class="c-degree-program-preview__title"
-         id="<?= esc_attr($titleId) ?>"
-    >
-        <?= esc_html($degreeProgram->title()) ?>
-        (<abbr title="<?= esc_attr($degreeProgram->degree()->name()) ?>"><?=
-            esc_html($degreeProgram->degree()->abbreviation())
-        ?></abbr>)
+    <div class="c-degree-program-preview__title">
+        <a
+            class="c-degree-program-preview__link"
+            href="<?= esc_url($link) ?>"
+            rel="bookmark"
+            aria-labelledby="<?= esc_attr($titleId) ?>"
+        ></a>
 
-        <div class="c-degree-program-preview__subtitle">
-            <?= esc_html($degreeProgram->subtitle()) ?>
+        <div id="<?= esc_attr($titleId) ?>">
+            <?= esc_html($degreeProgram->title()) ?>
+            (<abbr title="<?= esc_attr($degreeProgram->degree()->name()) ?>"><?=
+                esc_html($degreeProgram->degree()->abbreviation())
+            ?></abbr>)
+
+            <div class="c-degree-program-preview__subtitle">
+                <?= esc_html($degreeProgram->subtitle()) ?>
+            </div>
         </div>
     </div>
 
