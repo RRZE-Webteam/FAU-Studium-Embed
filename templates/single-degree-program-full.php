c20b0ca6e76f61c458095c1b3eee87759116fe68
inpsyde-deploybot <github-deploybot@inpsyde.com>
2023-07-24 07:06:43 +0000

    [SYNC] Merge pull request #123 from inpsyde/FAU-346

--- templates/single-degree-program-full.php
+++ templates/single-degree-program-full.php
@@ -22,11 +22,12 @@ use Fau\DegreeProgram\Common\Infrastructure\TemplateRenderer\Renderer;
 
 ?>
 
-<div class="c-single-degree-program <?= esc_attr($className) ?>"
+<section class="c-single-degree-program <?= esc_attr($className) ?>"
      lang="<?= esc_attr(get_bloginfo('language')) ?>"
 >
     <?php // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped ?>
     <?= $renderer->render('single-degree-program/heading', ['view' => $view]) ?>
+
     <?= $renderer->render('single-degree-program/featured-image', ['view' => $view]) ?>
     <?= $renderer->render('single-degree-program/entry-text', ['view' => $view]) ?>
     <?= $renderer->render('single-degree-program/details', ['view' => $view]) ?>
@@ -40,4 +41,4 @@ use Fau\DegreeProgram\Common\Infrastructure\TemplateRenderer\Renderer;
     <?= $renderer->render('single-degree-program/more-information', ['view' => $view]) ?>
     <?= $renderer->render('single-degree-program/links', ['view' => $view]) ?>
     <?php // phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-</div>
+</section>
