5dafc9bb20aac8d8a727da4685e14d8dc6a0a0c2
inpsyde-deploybot <github-deploybot@inpsyde.com>
2023-07-27 08:03:48 +0000

    [BOT] Build package

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'inpsyde/fau-degree-program-output',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '97db97520634bba9274d04c9ee6a47854d18c65e',
+        'reference' => '62cfe308db82600bfc1df1fccda4a5c87a34701e',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -33,7 +33,7 @@
         'inpsyde/fau-degree-program-output' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '97db97520634bba9274d04c9ee6a47854d18c65e',
+            'reference' => '62cfe308db82600bfc1df1fccda4a5c87a34701e',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
