3339d8653208dc3eb84bf79636a179b05033e0d7
Oleksandr Zhyian <oleksandr.zhyian@gmail.com>
2024-05-07 18:26:13 +0300

    feat: trigger search whenever any filter dropdown is closed

--- resources/ts/form/input-handler.ts
+++ resources/ts/form/input-handler.ts
@@ -1,24 +1,31 @@
 import submitForm from './form-handler';
+import isReducedMotion from '../common/reduced-motion-detection';
 
-const INPUT_SELECTOR = '.c-degree-programs-sarchform__input';
-const INPUT_DELAY = 2000;
-const MIN_CHARACTERS = 3;
+const init = () => {
+	const INPUT_SELECTOR = '.c-degree-programs-sarchform__input';
+	const INPUT_DELAY = 1500;
+	const MIN_CHARACTERS = 3;
 
-const input = document.querySelector( INPUT_SELECTOR ) as HTMLInputElement;
+	const input = document.querySelector( INPUT_SELECTOR ) as HTMLInputElement;
 
-let timeout: ReturnType< typeof setTimeout > | null = null;
+	let timeout: ReturnType< typeof setTimeout > | null = null;
 
-input?.addEventListener( 'input', () => {
-	if ( timeout ) {
-		clearTimeout( timeout );
-	}
+	input?.addEventListener( 'input', () => {
+		if ( timeout ) {
+			clearTimeout( timeout );
+		}
 
-	const inputValue = input.value.trim();
+		const inputValue = input.value.trim();
 
-	if ( inputValue.length > MIN_CHARACTERS ) {
-		timeout = setTimeout( () => {
-			submitForm();
-			timeout = null;
-		}, INPUT_DELAY );
-	}
-} );
+		if ( inputValue.length > MIN_CHARACTERS ) {
+			timeout = setTimeout( () => {
+				submitForm();
+				timeout = null;
+			}, INPUT_DELAY );
+		}
+	} );
+};
+
+if ( ! isReducedMotion() ) {
+	init();
+}
