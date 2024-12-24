4785b32a2b9b381c981464f0cb29f0ad365abea3
Oleksandr Zhyian <oleksandr.zhyian@gmail.com>
2024-12-24 15:24:54 +0200

    feat: synchronize parent and child terms using the MultilingualLink representation

--- src/Infrastructure/Search/FilterableTermsUpdater.php
+++ src/Infrastructure/Search/FilterableTermsUpdater.php
@@ -341,7 +341,7 @@ final class FilterableTermsUpdater
             return 0;
         }
 
-        /** @var AdmissionRequirement|Degree $flatProperty */
+        /** @var AdmissionRequirement|Degree|MultilingualLink $flatProperty */
         $parentProperty = $flatProperty->parent();
 
         if (is_null($parentProperty)) {
@@ -403,7 +403,11 @@ final class FilterableTermsUpdater
         MultilingualString|MultilingualLink|AdmissionRequirement|Degree $flatProperty
     ): bool {
 
-        return $flatProperty instanceof Degree || $flatProperty instanceof AdmissionRequirement;
+        return in_array($flatProperty::class, [
+            Degree::class,
+            AdmissionRequirement::class,
+            MultilingualLink::class,
+        ], true);
     }
 
     private function createTerm(TermData $termData): ?int
